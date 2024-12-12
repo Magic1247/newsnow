4022fa9d1faf76b66e46bac2f7b399db2ada6d04
loki.zuo <zyhttx@Gmail.com>
2024-12-12 10:38:28 +0800

    完成自研

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -46,7 +46,7 @@ export function Header() {
         <Link to="/" className="flex gap-2 items-center">
           <div className="h-10 w-10 bg-cover" title="logo" style={{ backgroundImage: "url(/icon.svg)" }} />
           <span className="text-2xl font-brand line-height-none!">
-            <p>News</p>
+            <p>ArkNews</p>
             <p className="mt--1">
               <span className="color-primary-6">N</span>
               <span>ow</span>
